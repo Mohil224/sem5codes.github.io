9a09ceffe31c09cc2776a9b09fbf9a41e6b925a8
Mohil224 <89787398+Mohil224@users.noreply.github.com>
2022-09-19 17:19:47 +0530

    first

---
+++ Contents/Rolling_a_ball/Assets/Scripts/rotator.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour {
+    private void Start() { }
+    public AudioSource onCollect;
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+}
